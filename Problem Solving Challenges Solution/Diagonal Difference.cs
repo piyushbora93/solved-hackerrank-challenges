5cd88552223aa77a5523b939f838a8bdf3b55bdf
piyushbora93 <88310944+piyushbora93@users.noreply.github.com>
2021-08-02 13:18:56 +0530

    Create Diagonal Difference.cs

---
+++ Problem Solving Challenges Solution/Diagonal Difference.cs
@@ -0,0 +1,57 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int diagonalDifference(List<List<int>> arr)
+    {
+        int numberofList = arr.Count();
+        int i = 0;
+        int j = numberofList - 1;
+        int primarySum = 0;
+        int secondarySum = 0;
+        while(i < numberofList && j > -1){
+            primarySum += arr[i][i];
+            secondarySum += arr[j][i];
+            i++;
+            j--;
+        }
+        return Math.Abs(primarySum - secondarySum);            
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
