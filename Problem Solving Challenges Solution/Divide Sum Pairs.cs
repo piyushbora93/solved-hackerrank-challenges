79f57a325dd553938d02ec3541d1dc74831b48c2
piyushbora93 <88310944+piyushbora93@users.noreply.github.com>
2021-08-04 01:12:02 +0530

    Create Divide Sum Pairs.cs

---
+++ Problem Solving Challenges Solution/Divide Sum Pairs.cs
@@ -0,0 +1,55 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{    
+    public static int divisibleSumPairs(int n, int k, List<int> ar)
+    {
+        int[] inputArr = ar.ToArray();
+        int result = 0;
+        for(int i = 0; i < inputArr.Length; i++){
+            for(int j = i + 1; j < inputArr.Length; j++){
+                int sum = inputArr[i] + inputArr[j];
+                if(sum % k == 0){
+                    result++;
+                }
+            }
+        }
+        return result;                 
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
