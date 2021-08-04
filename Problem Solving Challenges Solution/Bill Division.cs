8f02a460f4256fe0733acb5751ef4003d7b49b3a
piyushbora93 <88310944+piyushbora93@users.noreply.github.com>
2021-08-04 23:51:51 +0530

    Create Bill Division.cs

---
+++ Problem Solving Challenges Solution/Bill Division.cs
@@ -0,0 +1,46 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static void bonAppetit(List<int> bill, int k, int b)
+    {        
+        int totalBill = (bill.Sum() - bill[k]) / 2;        
+        if(totalBill == b){
+            Console.WriteLine("Bon Appetit");
+        }
+        else{
+            Console.WriteLine(b - totalBill);
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+        int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.bonAppetit(bill, k, b);
+    }
+}
