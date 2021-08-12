686245c13e44b9af9e3bc1a38905052d66b1d34d
piyushbora93 <88310944+piyushbora93@users.noreply.github.com>
2021-08-12 19:15:49 +0530

    Create Beautiful Day At The Movies.cs

---
+++ Problem Solving Challenges Solution/Beautiful Day At The Movies.cs
@@ -0,0 +1,51 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int beautifulDays(int i, int j, int k)
+    {
+        int beautifulDaysCount = 0;
+        for(int a = i; a <= j; a++){
+            if((Math.Abs(a - a.ToString().Reverse().Aggregate(0, (b, x) => 10 * b + x - '0'))) % k == 0){
+                beautifulDaysCount++;
+            }
+        };   
+        return beautifulDaysCount;     
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int i = Convert.ToInt32(firstMultipleInput[0]);
+
+        int j = Convert.ToInt32(firstMultipleInput[1]);
+
+        int k = Convert.ToInt32(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
