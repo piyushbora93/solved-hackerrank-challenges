d6b37ddc16b562cce29fd092fecf454b74df62df
piyushbora93 <88310944+piyushbora93@users.noreply.github.com>
2021-08-12 18:52:35 +0530

    Create Utopian Tree.cs

---
+++ Problem Solving Challenges Solution/Utopian Tree.cs
@@ -0,0 +1,54 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    public static int utopianTree(int n)
+    {
+        int finalHeight = 1;
+        for(int i = 1; i < n + 1; i++){
+            if(i % 2 == 0){
+                finalHeight++;
+            }
+            else{
+                finalHeight = 2 * finalHeight;
+            }            
+        }
+        return finalHeight;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.utopianTree(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
