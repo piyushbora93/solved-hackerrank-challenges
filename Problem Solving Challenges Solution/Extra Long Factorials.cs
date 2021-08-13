efafc5a819e765d3fd60e771fa878fa68f7fc3b6
piyushbora93 <88310944+piyushbora93@users.noreply.github.com>
2021-08-13 20:04:32 +0530

    Create Extra Long Factorials.cs

---
+++ Problem Solving Challenges Solution/Extra Long Factorials.cs
@@ -0,0 +1,39 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    
+
+    public static void extraLongFactorials(int n)
+    {
+        System.Numerics.BigInteger result = 1;
+        while(n > 0){
+            result = result * n;
+            n--;
+        }
+        Console.WriteLine(result);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.extraLongFactorials(n);
+    }
+}
