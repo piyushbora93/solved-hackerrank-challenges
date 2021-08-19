868042bb4151510ae56add3d07ca9efa17deda29
piyushbora93 <88310944+piyushbora93@users.noreply.github.com>
2021-08-19 20:17:10 +0530

    Create Between Two Sets.cs

---
+++ Problem Solving Challenges Solution/Between Two Sets.cs
@@ -0,0 +1,59 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{    
+
+    public static int getTotalX(List<int> a, List<int> b)
+    {
+        int result = 0;
+        int initialValue = a.Max();
+        
+        while(initialValue <= b.Min()){
+            int firstCount = a.Where(i => initialValue % i == 0).Count();
+            int secondCount = b.Where(i => i % initialValue == 0).Count();            
+            if(firstCount == a.Count && secondCount == b.Count){
+                result++;                
+            }            
+            initialValue++;
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        List<int> brr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+        int total = Result.getTotalX(arr, brr);
+
+        textWriter.WriteLine(total);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
