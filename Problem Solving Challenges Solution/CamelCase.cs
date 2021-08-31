68369a09c711a8ba541e66f7752922bb75466c75
piyushbora93 <88310944+piyushbora93@users.noreply.github.com>
2021-08-31 23:05:50 +0530

    Create CamelCase.cs

---
+++ Problem Solving Challenges Solution/CamelCase.cs
@@ -0,0 +1,46 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{    
+    public static int camelcase(string s)
+    {
+        int result = 1;
+        for(int i =0; i < s.Length; i++){
+            char x = s[i];
+            if(char.IsUpper(x)){
+                result += 1;
+            }
+        }
+        return result;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        int result = Result.camelcase(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
